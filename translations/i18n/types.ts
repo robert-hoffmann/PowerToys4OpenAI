41363a75048ea1c7ec2b63847e4e0f4873aa9760
Robert Hoffmann <mail.someone@gmail.com>
2023-04-18 11:34:43 +0200

    Create types.ts

---
+++ translations/i18n/types.ts
@@ -0,0 +1,96 @@
+export const translation = {
+  messages: {
+    hello            : 'messages.hello',
+    copiedToClipBoard: 'messages.copiedToClipBoard',
+  },
+  ui: {
+    me         : 'ui.me',
+    save       : 'ui.save',
+    close      : 'ui.close',
+    play       : 'ui.play',
+    record     : 'ui.record',
+    import     : 'ui.import',
+    export     : 'ui.export',
+    cancel     : 'ui.cancel',
+    delete     : 'ui.delete',
+    missing    : 'ui.missing',
+    search     : 'ui.search',
+    searchHint : 'ui.searchHint',
+    welcome    : 'ui.welcome',
+    authError  : 'ui.authError',
+    comingSoon : 'ui.comingSoon',
+    donate     : 'ui.donate',
+    donateCopy : 'ui.donateCopy'
+  },
+  searchCompanion: {
+    title      : 'searchCompanion.title',
+    login      : 'searchCompanion.login',
+    retry      : 'searchCompanion.retry',
+    search     : 'searchCompanion.search',
+    searchHint : 'searchCompanion.searchHint',
+    settings   : 'searchCompanion.settings',
+    clear      : 'searchCompanion.clear',
+    copy       : 'searchCompanion.copy',
+    cancel     : 'searchCompanion.cancel'
+  },
+  components: {
+    conversations: {
+      title           : 'components.conversations.title',
+      newConversation : 'components.conversations.newConversation'
+    },
+    profiles: {
+      title        : 'components.profiles.title',
+      newProfile   : 'components.profiles.newProfile',
+      selectProfile: 'components.profiles.selectProfile',
+      upload       : 'components.profiles.upload',
+      cancel       : 'components.profiles.cancel',
+      selected     : 'components.profiles.selected'
+    },
+    settings: {
+      settings   : 'components.settings.settings',
+      general    : 'components.settings.general',
+      global     : 'components.settings.global'
+    }
+  },
+  pages : {
+    profiles :  {
+      title       : 'pages.profiles.title',
+      active      : 'pages.profiles.active',
+      tags        : 'pages.profiles.tags',
+      description : 'pages.profiles.description',
+      prompt      : 'pages.profiles.prompt',
+    }
+  },
+  help: {
+    intro1        : 'help.intro1',
+    intro2        : 'help.intro2',
+    intro3        : 'help.intro3',
+    step1Title    : 'help.step1Title',
+    step1Content1 : 'help.step1Content1',
+    step2Title    : 'help.step2Title',
+    step2Content1 : 'help.step2Content1',
+    step2Content2 : 'help.step2Content2',
+    step2Content3 : 'help.step2Content3',
+    step3Title    : 'help.step3Title',
+    step3Content1 : 'help.step3Content1',
+    step3Content2 : 'help.step3Content2',
+    stepperNext   : 'help.stepperNext',
+    stepperBack   : 'help.stepperBack',
+    stepperFinish : 'help.stepperFinish'
+  },
+  settings: {
+    searchCompanion: {
+      title                   : 'settings.searchCompanion.title',
+      interfaceLanguage       : 'settings.searchCompanion.interfaceLanguage',
+      triggerModeTitle        : 'settings.searchCompanion.triggerModeTitle',
+      triggerModeManual       : 'settings.searchCompanion.triggerModeManual',
+      triggerModeManualHint   : 'settings.searchCompanion.triggerModeManualHint',
+      triggerModeQuestion     : 'settings.searchCompanion.triggerModeQuestion',
+      triggerModeQuestionHint : 'settings.searchCompanion.triggerModeQuestionHint',
+      triggerModeAuto         : 'settings.searchCompanion.triggerModeAuto',
+      triggerModeAutoHint     : 'settings.searchCompanion.triggerModeAutoHint',
+      apiKeyTitle             : 'settings.searchCompanion.apiKeyTitle',
+      apiKeyHint              : 'settings.searchCompanion.apiKeyHint'
+    }
+  }
+};
