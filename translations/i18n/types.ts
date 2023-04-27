3a3a7b5dd54f1910ee0d028fdeac060c80679dad
Robert Hoffmann <mail.someone@gmail.com>
2023-04-27 09:25:07 +0200

    Update types.ts

--- translations/i18n/types.ts
+++ translations/i18n/types.ts
@@ -21,55 +21,49 @@ export const translation = {
     donateCopy        : 'global.donateCopy',
     copiedToClipBoard : 'global.copiedToClipBoard',
   },
-  components: {
-    searchCompanion: {
-      login      : 'components.searchCompanion.login',
-      search     : 'components.searchCompanion.search',
-      searchHint : 'components.searchCompanion.searchHint',
-      clear      : 'components.searchCompanion.clear',
-      copy       : 'components.searchCompanion.copy',
-    },
-    conversations: {
-      me              : 'components.conversations.me',
-      welcome         : 'components.conversations.welcome',
-      title           : 'components.conversations.title',
-      newConversation : 'components.conversations.newConversation',
-      sttHint         : 'components.conversations.sttHint',
-      sttOn           : 'components.conversations.sttOn',
-      sttOff          : 'components.conversations.sttOff',
-      inputHint       : 'components.conversations.inputHint',
-      wordCount       : 'components.conversations.wordCount',
-    },
-    profiles: {
-      title        : 'components.profiles.title',
-      newProfile   : 'components.profiles.newProfile',
-      selectProfile: 'components.profiles.selectProfile',
-      selected     : 'components.profiles.selected'
-    },
-    settings: {
-      general : 'components.settings.general',
-      global  : 'components.settings.global'
-    },
-    searchSettings: {
-      interfaceLanguage       : 'components.searchSettings.interfaceLanguage',
-      triggerModeTitle        : 'components.searchSettings.triggerModeTitle',
-      triggerModeManual       : 'components.searchSettings.triggerModeManual',
-      triggerModeManualHint   : 'components.searchSettings.triggerModeManualHint',
-      triggerModeQuestion     : 'components.searchSettings.triggerModeQuestion',
-      triggerModeQuestionHint : 'components.searchSettings.triggerModeQuestionHint',
-      triggerModeAuto         : 'components.searchSettings.triggerModeAuto',
-      triggerModeAutoHint     : 'components.searchSettings.triggerModeAutoHint',
-      apiKeyTitle             : 'components.searchSettings.apiKeyTitle',
-      apiKeyHint              : 'components.searchSettings.apiKeyHint'
-    }
+  searchCompanion: {
+    login      : 'components.searchCompanion.login',
+    search     : 'components.searchCompanion.search',
+    searchHint : 'components.searchCompanion.searchHint',
+    clear      : 'components.searchCompanion.clear',
+    copy       : 'components.searchCompanion.copy',
   },
-  pages : {
-    profiles :  {
-      title       : 'pages.profiles.title',
-      active      : 'pages.profiles.active',
-      tags        : 'pages.profiles.tags',
-      description : 'pages.profiles.description',
-      prompt      : 'pages.profiles.prompt',
-    }
+  conversations: {
+    me              : 'components.conversations.me',
+    welcome         : 'components.conversations.welcome',
+    title           : 'components.conversations.title',
+    newConversation : 'components.conversations.newConversation',
+    sttHint         : 'components.conversations.sttHint',
+    sttOn           : 'components.conversations.sttOn',
+    sttOff          : 'components.conversations.sttOff',
+    inputHint       : 'components.conversations.inputHint',
+    wordCount       : 'components.conversations.wordCount',
   },
+  profiles: {
+    title         : 'components.profiles.title',
+    newProfile    : 'components.profiles.newProfile',
+    selectProfile : 'components.profiles.selectProfile',
+    selected      : 'components.profiles.selected',
+    name          : 'components.profiles.name',
+    active        : 'components.profiles.active',
+    tags          : 'components.profiles.tags',
+    description   : 'components.profiles.description',
+    prompt        : 'components.profiles.prompt',
+  },
+  settings: {
+    general : 'components.settings.general',
+    global  : 'components.settings.global'
+  },
+  searchSettings: {
+    interfaceLanguage       : 'components.searchSettings.interfaceLanguage',
+    triggerModeTitle        : 'components.searchSettings.triggerModeTitle',
+    triggerModeManual       : 'components.searchSettings.triggerModeManual',
+    triggerModeManualHint   : 'components.searchSettings.triggerModeManualHint',
+    triggerModeQuestion     : 'components.searchSettings.triggerModeQuestion',
+    triggerModeQuestionHint : 'components.searchSettings.triggerModeQuestionHint',
+    triggerModeAuto         : 'components.searchSettings.triggerModeAuto',
+    triggerModeAutoHint     : 'components.searchSettings.triggerModeAutoHint',
+    apiKeyTitle             : 'components.searchSettings.apiKeyTitle',
+    apiKeyHint              : 'components.searchSettings.apiKeyHint'
+  }
 };
