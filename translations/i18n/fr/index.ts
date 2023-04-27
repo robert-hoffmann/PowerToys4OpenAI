2831c2cb83fee6fe5c2a7aca25991fb6419c367f
Robert Hoffmann <mail.someone@gmail.com>
2023-04-27 09:25:59 +0200

    Update index.ts

--- translations/i18n/fr/index.ts
+++ translations/i18n/fr/index.ts
@@ -22,57 +22,51 @@ export default {
       donateCopy        : 'Adresse {{name}} copié dans le presse-papier',
       copiedToClipBoard : 'Contenu copié dans le presse-papier',
     },
-    components: {
-      searchCompanion: {
-        title      : 'Compagnon de recherche',
-        login      : 'Connexion',
-        search     : 'Chercher',
-        searchHint : 'Développer ce sujet',
-        clear      : 'Effacer l\'hitorique des conversations',
-        copy       : 'Copier le résultat',
-      },
-      conversations: {
-        me              : 'moi',
-        welcome         : 'Bonjour, comment puis-je vous aider',
-        title           : 'Conversations',
-        newConversation : 'Nouvelle conversation',
-        sttHint         : 'Vous pouvez l\'activer/désactiver avec CTRL+Espace',
-        sttOn           : 'Reconnaissance Vocale: Activée',
-        sttOff          : 'Reconnaissance Vocale: Désactivée',
-        inputHint       : 'Vous pouvez valider avec CTRL+Entrée, ou utiliser la reconnaissance vocale avec CTRL+Espace',
-        wordCount       : 'Nombre de mots',
-      },
-      profiles: {
-        title         : 'Profils',
-        newProfile    : 'Nouveau profil',
-        selectProfile : 'Sélectionner un profil',
-        selected      : 'Profil Actif: {{name}}'
-      },
-      settings: {
-        general : 'Général',
-        global  : 'Paramètres globaux'
-      },
-      searchSettings: {
-        interfaceLanguage       : 'Langue de l\'interface',
-        triggerModeTitle        : 'Mode de déclenchement',
-        triggerModeManual       : 'Manuel',
-        triggerModeManualHint   : 'Déclenchement uniquement sur clic manuel',
-        triggerModeQuestion     : 'Question',
-        triggerModeQuestionHint : 'Déclenchement uniquement si se termine par : ?',
-        triggerModeAuto         : 'Automatique',
-        triggerModeAutoHint     : 'Interroger systématiquement le moteur de recherche',
-        apiKeyTitle             : 'Clé API',
-        apiKeyHint              : 'Votre clé API OpenAI',
-      }
+    searchCompanion: {
+      title      : 'Compagnon de recherche',
+      login      : 'Connexion',
+      search     : 'Chercher',
+      searchHint : 'Développer ce sujet',
+      clear      : 'Effacer l\'hitorique des conversations',
+      copy       : 'Copier le résultat',
     },
-    pages : {
-      profiles: {
-        title       : 'Profils',
-        active      : 'Actif',
-        tags        : 'Tags',
-        description : 'Déscription',
-        prompt      : 'Invité de commande'
-      }
+    conversations: {
+      me              : 'moi',
+      welcome         : 'Bonjour, comment puis-je vous aider',
+      title           : 'Conversations',
+      newConversation : 'Nouvelle conversation',
+      sttHint         : 'Vous pouvez l\'activer/désactiver avec CTRL+Espace',
+      sttOn           : 'Reconnaissance Vocale: Activée',
+      sttOff          : 'Reconnaissance Vocale: Désactivée',
+      inputHint       : 'Vous pouvez valider avec CTRL+Entrée, ou utiliser la reconnaissance vocale avec CTRL+Espace',
+      wordCount       : 'Nombre de mots',
     },
+    profiles: {
+      title         : 'Profils',
+      newProfile    : 'Nouveau profil',
+      selectProfile : 'Sélectionner un profil',
+      selected      : 'Profil Actif: {{name}}',
+      name          : 'Profils',
+      active        : 'Actif',
+      tags          : 'Tags',
+      description   : 'Déscription',
+      prompt        : 'Invité de commande'
+    },
+    settings: {
+      general : 'Général',
+      global  : 'Paramètres globaux'
+    },
+    searchSettings: {
+      interfaceLanguage       : 'Langue de l\'interface',
+      triggerModeTitle        : 'Mode de déclenchement',
+      triggerModeManual       : 'Manuel',
+      triggerModeManualHint   : 'Déclenchement uniquement sur clic manuel',
+      triggerModeQuestion     : 'Question',
+      triggerModeQuestionHint : 'Déclenchement uniquement si se termine par : ?',
+      triggerModeAuto         : 'Automatique',
+      triggerModeAutoHint     : 'Interroger systématiquement le moteur de recherche',
+      apiKeyTitle             : 'Clé API',
+      apiKeyHint              : 'Votre clé API OpenAI',
+    }
   }
 };
