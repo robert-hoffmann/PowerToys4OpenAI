eea80cf7f78b235283f1c5bc8a5b7bbed34dd222
Robert Hoffmann <mail.someone@gmail.com>
2023-04-27 09:25:36 +0200

    Update index.ts

--- translations/i18n/en/index.ts
+++ translations/i18n/en/index.ts
@@ -22,58 +22,52 @@ export default {
       donateCopy        : '{{name}} address copied to clipboard',
       copiedToClipBoard : 'Content copied to clipboard',
     },
-    components: {
-      searchCompanion: {
-        title      : 'Search Companion',
-        login      : 'Login',
-        search     : 'Search',
-        searchHint : 'Expand on this topic',
-        clear      : 'Clear conversation history',
-        copy       : 'Copy the result',
-      },
-      conversations: {
-        me              : 'me',
-        welcome         : 'Hi there, how may i assist you',
-        title           : 'Conversations',
-        newConversation : 'New conversation',
-        sttHint         : 'Toggle listening with CTRL+SpaceBar',
-        sttOn           : 'Speech Recognition: Activated',
-        sttOff          : 'Speech Recognition: Deactivated',
-        inputHint       : 'You can validate with CTRL+Enter, or use speech recognition with CTRL+SpaceBar',
-        wordCount       : 'Word count',
-      },
-      profiles: {
-        title        : 'Profiles',
-        newProfile   : 'New profile',
-        selectProfile: 'Select a profile',
-        selected     : 'Active Profile: {{name}}'
+    searchCompanion: {
+      title      : 'Search Companion',
+      login      : 'Login',
+      search     : 'Search',
+      searchHint : 'Expand on this topic',
+      clear      : 'Clear conversation history',
+      copy       : 'Copy the result',
+    },
+    conversations: {
+      me              : 'me',
+      welcome         : 'Hi there, how may i assist you',
+      title           : 'Conversations',
+      newConversation : 'New conversation',
+      sttHint         : 'Toggle listening with CTRL+SpaceBar',
+      sttOn           : 'Speech Recognition: Activated',
+      sttOff          : 'Speech Recognition: Deactivated',
+      inputHint       : 'You can validate with CTRL+Enter, or use speech recognition with CTRL+SpaceBar',
+      wordCount       : 'Word count',
+    },
+    profiles: {
+      title         : 'Profiles',
+      newProfile    : 'New profile',
+      selectProfile : 'Select a profile',
+      selected      : 'Active Profile: {{name}}',
+      name          : 'Title',
+      active        : 'Active',
+      tags          : 'Tags',
+      description   : 'Description',
+      prompt        : 'System prompt',
 
-      },
-      settings: {
-        general : 'General',
-        global  : 'Global settings'
-      },
-      searchSettings: {
-        interfaceLanguage       : 'Interface language',
-        triggerModeTitle        : 'Trigger Mode',
-        triggerModeManual       : 'Manual',
-        triggerModeManualHint   : 'Only trigger search on manual click',
-        triggerModeQuestion     : 'Question',
-        triggerModeQuestionHint : 'Only trigger search if ending with: ?',
-        triggerModeAuto         : 'Automatic',
-        triggerModeAutoHint     : 'Systematically query the search engine',
-        apiKeyTitle             : 'API Key',
-        apiKeyHint              : 'Your OpenAI API key'
-      }
     },
-    pages : {
-      profiles :  {
-        title       : 'Title',
-        active      : 'Active',
-        tags        : 'Tags',
-        description : 'Description',
-        prompt      : 'System prompt',
-      }
+    settings: {
+      general : 'General',
+      global  : 'Global settings'
     },
+    searchSettings: {
+      interfaceLanguage       : 'Interface language',
+      triggerModeTitle        : 'Trigger Mode',
+      triggerModeManual       : 'Manual',
+      triggerModeManualHint   : 'Only trigger search on manual click',
+      triggerModeQuestion     : 'Question',
+      triggerModeQuestionHint : 'Only trigger search if ending with: ?',
+      triggerModeAuto         : 'Automatic',
+      triggerModeAutoHint     : 'Systematically query the search engine',
+      apiKeyTitle             : 'API Key',
+      apiKeyHint              : 'Your OpenAI API key'
+    }
   }
 };
